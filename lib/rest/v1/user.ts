48d01cb15989606f94efc76ae83ae8787e6580ae
Colderry <c0l4e6r7@gmail.com>
2022-05-14 15:49:50 +0600

    feat(user): add comments and user summary types

--- lib/rest/v1/user.ts
+++ lib/rest/v1/user.ts
@@ -1,10 +1,32 @@
-import { APIUser, APIUserTypes } from "../../payloads";
+/**
+ * @link
+ * https://www.guilded.gg/docs/api/members/User
+ */
 
-export type RestGetAPIUser = APIUser;
-
-export interface RestGetAPIUserSummary {
+ export interface APIUser {
+    /** The ID of the user */
     id: string;
+    /**
+     * The type of user.
+     * If this property is absent, it can assumed to be of type user
+     */
     type?: APIUserTypes;
+    /** The name of the user */
     name: string;
+    /** The avatar image associated with the user */
     avatar?: string;
+    /** The banner image associated with the user */
+    banner?: string;
+    /** The ISO 8601 timestamp that the user was created at */
+    createdAt: string;
+};
+
+export type APIUserTypes = "bot" | "user";
+
+/**
+ * https://www.guilded.gg/docs/api/members/UserSummary
+ */
+export interface APIUserSummary extends Omit<APIUser, "banner" | "createdAt"> {
+    /** The ID of the user */
+    id: string;
 };
